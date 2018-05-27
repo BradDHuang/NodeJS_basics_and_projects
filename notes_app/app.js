7a382ce989cc71ae009ee8cfd59d9f62742c3e39
BradDHuang <adghbc01@tamu.edu>
2018-05-27 22:35:27 +0000

    module module.exports

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,7 +1,8 @@
-console.log("starting the notes app...");
+console.log("starting app.js");
 
 const fs = require('fs'); // the file system
 const os = require('os');
+const notes = require('./notes.js');
 /*
 // Create a file(if not already exists) & append the content to it:
 // fs.appendFile(path, data[, options], callback)
@@ -11,16 +12,23 @@ fs.appendFile("greetings.txt", "Hi Brad.", function(err) {
     }
 });
 */
-
+/*
 var user = os.userInfo();
 // console.log(user);
 // fs.appendFile("greetings.txt", "Hi " + user.username + ".", function(err) {
-fs.appendFile("greetings.txt", `Hi ${user.username}.`, function(err) { // ES6.
+// fs.appendFile("greetings.txt", `Hi ${user.username}.`, function(err) { // ES6.
+fs.appendFile("greetings.txt", `Hi ${user.username}, you are of age ${notes.age}.`, function(err) { // ES6.
     if (err) {
         console.log("unable to write to file!");
     }
 });
+*/
 
+// var res = notes.addNote();
+// console.log(res); // "new note"
+
+var sum = notes.add(3, 2); // 5;
+console.log(sum);
 
 
 
---
+++ notes_app/notes.js
@@ -0,0 +1,28 @@
+console.log("starting notes.js");
+
+// module
+// console.log(module); // Module {...}
+/*
+// ******
+// exports: {}
+// console.log(module.exports); 
+// add a property to it e.g.:
+// module.exports.age = 24;
+// module.exports.addNote = function() { // ES5
+module.exports.addNote = () => { // Arrow func.
+    console.log("addNote");
+    return "new note";
+};
+// console.log(module.exports); 
+// ******
+*/
+
+module.exports.add = (a, b) => {
+    return a + b;
+};
+
+
+
+
+
+
