82b071fe457b72359b6ddb37301351d07a234d8a
BradDHuang <adghbc01@tamu.edu>
2018-05-27 21:41:20 +0000

    // ES6

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,7 +1,8 @@
 console.log("starting the notes app...");
 
 const fs = require('fs'); // the file system
-
+const os = require('os');
+/*
 // Create a file(if not already exists) & append the content to it:
 // fs.appendFile(path, data[, options], callback)
 fs.appendFile("greetings.txt", "Hi Brad.", function(err) {
@@ -9,6 +10,18 @@ fs.appendFile("greetings.txt", "Hi Brad.", function(err) {
         console.log("unable to write to file!");
     }
 });
+*/
+
+var user = os.userInfo();
+// console.log(user);
+// fs.appendFile("greetings.txt", "Hi " + user.username + ".", function(err) {
+fs.appendFile("greetings.txt", `Hi ${user.username}.`, function(err) { // ES6.
+    if (err) {
+        console.log("unable to write to file!");
+    }
+});
+
+
 
 
 
