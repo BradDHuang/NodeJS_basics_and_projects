20343f840cf499caf9b294736fe53e3e879d3332
BradDHuang <adghbc01@tamu.edu>
2018-05-28 09:48:40 +0000

    3rd party modules, e.g. lodash

--- notes_app/app.js
+++ notes_app/app.js
@@ -3,6 +3,18 @@ console.log("starting app.js");
 const fs = require('fs'); // the file system
 const os = require('os');
 const notes = require('./notes.js');
+
+// var _ = require('lodash');
+const _ = require('lodash');
+/*
+console.log(_.isString(true)); // false
+console.log(_.isString("str")); // true
+*/
+// Creates a duplicate-free version of an array:
+// _.uniq(array)
+var dupFreeArr = _.uniq(["brad", 1, "brad", 1, 2, 3]);
+console.log(dupFreeArr); // [ 'brad', 1, 2, 3 ]
+
 /*
 // Create a file(if not already exists) & append the content to it:
 // fs.appendFile(path, data[, options], callback)
@@ -26,10 +38,10 @@ fs.appendFile("greetings.txt", `Hi ${user.username}, you are of age ${notes.age}
 
 // var res = notes.addNote();
 // console.log(res); // "new note"
-
+/*
 var sum = notes.add(3, 2); // 5;
 console.log(sum);
-
+*/
 
 
 
