e5039fb82132de7f43cc1350c5a859524cf4bcd8
BradDHuang <adghbc01@tamu.edu>
2018-06-29 21:58:25 +0000

    Promise example with args.

--- weather_app/playground/promise.js
+++ weather_app/playground/promise.js
@@ -1,12 +1,32 @@
 
 // ES6 Promises:
 
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === "number" && typeof b === "number") {
+                resolve(a + b);
+            } else {
+                reject("Args must be numbers.");
+            }
+        }, 2000);
+    });
+};
+
+asyncAdd(5, 6).then((res) => {
+    console.log("Result: " + res);
+}, (err) => {
+    console.log(err);
+});
+
+
+/*
 var aPromise = new Promise((resolve, reject) => {
     // async
     // resolve("This is a Promise.");
     setTimeout(() => {
-        // resolve("This is a Promise.");
-        reject("The Promise was rejected."); // Unhandled Promise Rejection Warning.
+        resolve("This is a Promise.");
+        // reject("The Promise was rejected."); // Unhandled Promise Rejection Warning.
     }, 3000); // delay 3s.
 });
 
@@ -15,7 +35,7 @@ aPromise.then((msg) => {
 }, (err) => {
     console.log("Error: " + err);
 });
-
+*/
 
 
 
