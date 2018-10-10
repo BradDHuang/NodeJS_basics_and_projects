47fa31bb2c909e09f4aef3adb988bf6072b62537
BradDHuang <adghbc01@tamu.edu>
2018-10-10 22:16:58 +0000

    review: a Promise example.

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send({ hello: 'world' });
 });
 
+// Promise e.g.
+const wait = time => new Promise((resolve) => setTimeout(resolve, time));
+wait(3000).then(() => console.log('Hello!')); // 'Hello!'
+
 app.listen(port, () => {
   console.log(`Express app is runnung on port ${port}.`);
 });
