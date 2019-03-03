434d778c008e00f5c846811a5aeba1febd021686
BradDHuang <adghbc01@tamu.edu>
2019-03-03 19:41:16 +0000

    express (review)

---
+++ review.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const express = require('express')
+
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(path.join(__dirname, '../public'))
+
+const app = express()
+
+const publicDirPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirPath))
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello express</h1>')
+// })
+/*
+app.get('/help', (req, res) => {
+    res.send([{
+        name: 'Brad',
+        age: 25
+    }, {
+        name: 'Ben',
+        age: 25
+    }])
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About us</h1>')
+})
+*/
+app.get('/weather', (req, res) => {
+    // res.send({
+    //     forecast: 'rain',
+    //     location: 'Sunnyvale, CA'
+    // })
+    if (!req.query.location) {
+        return res.send({ err: 'Plz provide a location.'})
+    }
+    res.send({ 
+        forecast: 'cloudy',
+        location: req.query.location
+    })
+})
+
+app.get('/products', (req, res) => {
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000')
+})
+
